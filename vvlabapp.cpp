75a735ac2487ce5175aa5c150906b268bc41db56
Artem Bakanov <rtmbakanov@gmail.com>
2023-02-28 22:27:31 +0300

    added matrix input

--- vvlabapp.cpp
+++ vvlabapp.cpp
@@ -3,4 +3,28 @@
 #include <locale.h> //подключение файла русского языка
 #include <math.h> //вычисление корня
 #include <iostream> //использование потоков
+
 using namespace std;
+int** input(int n, int m)//ввод матрицы
+{
+	int i, j;
+	int** a;
+	a = (int**)malloc(m * sizeof(int*));
+	for (i = 0; i < m; i++)
+	{
+		a[i] = (int*)malloc(n * sizeof(int*));
+		for (j = 0; j < n; j++)
+		{
+			a[i][j] = 0;
+		}
+	}
+	for (i = 0; i < m; i++)
+	{
+		for (j = 0; j < n; j++)
+		{
+			printf("Введите элемент матрицы A[%d] [%d]: ", i + 1, j + 1);
+			scanf_s("%d", &a[i][j]);
+		}
+	}
+	return a;
+}
