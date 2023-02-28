1843655dd9da9aa15ccf99617d95cb55ac1996ab
Artem Bakanov <rtmbakanov@gmail.com>
2023-02-28 22:25:17 +0300

    created new app/installing C++ libraries

---
+++ vvlabapp.cpp
@@ -0,0 +1,6 @@
+#include <stdio.h> //библиотека ввода-вывода
+#include <stdlib.h> //использование динамической памяти
+#include <locale.h> //подключение файла русского языка
+#include <math.h> //вычисление корня
+#include <iostream> //использование потоков
+using namespace std;
